532296820a559f32190926ba4cbd423ee4b39287
harisanker10 <harisanker10@gmail.com>
2025-02-21 10:30:44 +0530

    fix: React-router issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router";
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <h1>Whitefield FC </h1>
+      <Link to="/admin/coaches">
+        <Button variant="outline"> Admin Panel </Button>
+      </Link>
     </main>
   );
 }
