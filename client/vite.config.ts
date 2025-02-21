fd2e91182f75583aca7cc948f01a3ff8872d26ae
harisanker10 <harisanker10@gmail.com>
2025-02-21 10:47:56 +0530

    fix: React-router issue

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,9 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["whitefieldfc-1.onrender.com"],
+  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
